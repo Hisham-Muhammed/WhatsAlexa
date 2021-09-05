43eb714d28703da766714824c8f69fa40afe6531
Hisham-Muhammed <87166264+Hisham-Muhammed@users.noreply.github.com>
2021-09-05 10:47:01 +0530

    Added inbox block

--- config.js
+++ config.js
@@ -32,6 +32,7 @@ module.exports = {
     BANMSG: process.env.BAN_MESSAGE === undefined ? 'default' : process.env.BAN_MESSAGE,
     BOT_PRESENCE: process.env.BOT_PRESENCE === undefined ? 'online' : process.env.BOT_PRESENCE,
     AFKMSG: process.env.AFK_MESSAGE === undefined ? 'default' : process.env.AFK_MESSAGE,
+    INBO: process.env.INBO_BLOCK === undefined ? 'true' : process.env.INBO_BLOCK,
     HANDLERS: process.env.HANDLERS === undefined ? '^[#.!]' : process.env.HANDLERS,
     SEND_READ: process.env.SEND_READ === undefined ? false : convertToBool(process.env.SEND_READ),
     BRANCH: 'master',
